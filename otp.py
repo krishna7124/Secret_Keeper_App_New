f46274deaf49a55f848f98710b0bbd872a6dbb5d
Krishna Bhatt <103425437+krishna7124@users.noreply.github.com>
2025-03-21 09:13:12 +0530

    Update otp.py

--- otp.py
+++ otp.py
@@ -1,30 +1,26 @@
-import streamlit as st
+import os
 import random
 import smtplib
 from email.mime.text import MIMEText
 import logging
 
-email_user = st.secrets["smtp"]["email_user"]
-email_password = st.secrets["smtp"]["email_password"]
+# Fetch SMTP credentials from Azure environment variables
+email_user = os.getenv("email_user")  
+email_password = os.getenv("email_password")
 
 # Generate a random OTP
-
-
 def generate_otp():
     otp = random.randint(100000, 999999)  # 6-digit OTP
     logging.info(f"Generated OTP: {otp}")
     return otp
 
 # Send OTP via email
-
-
 def send_otp_via_email(recipient_email, otp, purpose):
 
     if purpose == "login":
         try:
             msg = MIMEText(
                 f'''Hi, \n Your OTP for Login verification is: {otp} ''')
-            # msg = MIMEText(f"Your OTP for verification is: {otp}")
             msg['Subject'] = 'Secret Keeper App Login OTP'
             msg['From'] = email_user  # Sender's email
             msg['To'] = recipient_email
@@ -40,11 +36,11 @@ def send_otp_via_email(recipient_email, otp, purpose):
         except Exception as e:
             logging.error(f"Failed to send OTP: {e}")
             return False
+
     elif purpose == "signup":
         try:
             msg = MIMEText(
                 f'''Hi, Your OTP for Registration verification is: {otp} ''')
-            # msg = MIMEText(f"Your OTP for verification is: {otp}")
             msg['Subject'] = 'Secret Keeper App Registration OTP'
             msg['From'] = email_user  # Sender's email
             msg['To'] = recipient_email
@@ -60,6 +56,7 @@ def send_otp_via_email(recipient_email, otp, purpose):
         except Exception as e:
             logging.error(f"Failed to send OTP: {e}")
             return False
+
     else:
         logging.error("Registration Failed {e}.")
         return False
